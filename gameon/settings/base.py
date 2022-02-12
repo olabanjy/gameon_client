d7be4bfd7ee046d301c4c44c1167f719256b6469
Olabanji <shola.albert@gmail.com>
2022-02-12 11:23:04 +0100

    remove sensitive info

--- gameon/settings/base.py
+++ gameon/settings/base.py
@@ -12,7 +12,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-0wh0%c8%!4x%kxd!^wz#ku(%wk)12pdplkos1h4#t63v__)9l-"
 SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 
