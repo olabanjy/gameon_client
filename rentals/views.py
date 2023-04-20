b2032ea7b0af736b7812c2832821afddb5d4f4e0
Olabanji <shola.albert@gmail.com>
2023-04-20 08:51:34 +0100

    fixes

--- rentals/views.py
+++ rentals/views.py
@@ -540,8 +540,6 @@ def RentalGameDetails(request, item_id):
     all_other_items_qs = RentalGame.objects.exclude(id=item_id)
     range_items_id = [o.id for o in all_other_items_qs if o.checkInRadius(dest) == True]
     all_items = RentalGame.objects.filter(id__in=range_items_id)
-    for o in all_items:
-        print(o.name)
     context = {"item": item, "all_items": all_items}
     return render(request, template, context)
 
